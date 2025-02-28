501d0619175afcb1ab776bc2c0005d4288b08caa
Jeroen Stroobants <jeroenstroobants@gmail.com>
2025-02-28 10:30:22 +0100

    Remove instrumentation.ts

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { server } = await import("./app/api/cocktails/cocktails.json");
-    server.listen();
-  }
-}
