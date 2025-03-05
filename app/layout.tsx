516f1e3b34d74d18013358de551de1a814574bd9
Jeroen Stroobants <jeroenstroobants@gmail.com>
2025-03-05 20:59:42 +0100

    Fix font preloading warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import { Inter } from 'next/font/google';
 import { Navbar } from './components';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
